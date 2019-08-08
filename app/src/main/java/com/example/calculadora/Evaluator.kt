6bc0b6b03925cc4e79ff82379b133372e39fc0db
Lacour Vincent <lacour.vincent.app@gmail.com>
2019-08-08 21:42:22 +0200

    Perform calculation

---
+++ app/src/main/java/com/example/calculadora/Evaluator.kt
@@ -0,0 +1,39 @@
+package com.example.calculadora
+
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
+
+class Evaluator(var label: String) {
+
+    fun addElement(element: Char): String {
+        this.label += element
+        return this.label
+    }
+
+    fun removeLastElement(): String {
+        this.label = label.dropLast(1)
+        return this.label
+    }
+
+    fun clear(): String {
+        this.label = ""
+        return this.label
+    }
+
+    fun eval(): String {
+        try {
+            val convertedLabel = this.label
+                .replace("÷", "/")
+                .replace("×", "*")
+                .replace("%", "*0.01")
+            val expression = ExpressionBuilder(convertedLabel).build()
+            val res: Double = expression.evaluate()
+            return res.toString()
+        } catch (e: Exception) {
+            return "Error"
+        } finally {
+            this.label = ""
+        }
+    }
+
+}
--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -10,15 +10,52 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val evaluator = Evaluator("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_activity_main))
 
-        button_zero.setOnClickListener {
-            Toast.makeText(this, "0", Toast.LENGTH_SHORT).show()
-        }
+        button_zero.setOnClickListener { addElementToScreen('0') }
+        button_one.setOnClickListener { addElementToScreen('1') }
+        button_two.setOnClickListener { addElementToScreen('2') }
+        button_three.setOnClickListener { addElementToScreen('3') }
+        button_four.setOnClickListener { addElementToScreen('4') }
+        button_five.setOnClickListener { addElementToScreen('5') }
+        button_six.setOnClickListener { addElementToScreen('6') }
+        button_seven.setOnClickListener { addElementToScreen('7') }
+        button_eight.setOnClickListener { addElementToScreen('8') }
+        button_nine.setOnClickListener { addElementToScreen('9') }
 
+        button_comma.setOnClickListener { addElementToScreen('.') }
+        button_percentage.setOnClickListener { addElementToScreen('%') }
+
+        button_add.setOnClickListener { addElementToScreen('+') }
+        button_minus.setOnClickListener { addElementToScreen('-') }
+        button_multiply.setOnClickListener { addElementToScreen('×') }
+        button_divide.setOnClickListener { addElementToScreen('÷') }
+
+        button_equal.setOnClickListener { performCalculation() }
+
+        button_delete.setOnClickListener { removeLastElementToScreen() }
+        button_clear.setOnClickListener { clearScreen() }
+    }
+
+    fun addElementToScreen(element: Char) {
+        textview_screen.text = evaluator.addElement(element)
+    }
+
+    fun removeLastElementToScreen() {
+        textview_screen.text = evaluator.removeLastElement()
+    }
+
+    fun clearScreen() {
+        textview_screen.text = evaluator.clear()
+    }
+
+    fun performCalculation() {
+        textview_screen.text = evaluator.eval()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
