5aeb0c0e9607ca3061be911cd10e5922f7274943
Lacour Vincent <lacour.vincent.app@gmail.com>
2019-08-09 09:56:17 +0200

    Add unitTest on evaluator Class

--- app/src/androidTest/java/com/example/calculadora/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calculadora/ExampleInstrumentedTest.kt
@@ -1,18 +1,20 @@
-package com.example.calculadora
+/*package com.example.calculadora
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+*/
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
+ *//*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
@@ -21,4 +23,4 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.example.calculadora", appContext.packageName)
     }
-}
+}*/
---
+++ app/src/test/java/com/example/calculadora/EvaluatorTest.kt
@@ -0,0 +1,81 @@
+package com.example.calculadora
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class EvaluatorTest {
+
+    @Test
+    fun constructor() {
+        val evaluator = Evaluator("")
+        assertEquals("", evaluator.label)
+    }
+
+    @Test
+    fun addElement_emptyLabel() {
+        val evaluator = Evaluator("")
+        assertEquals("1", evaluator.addElement('1'))
+    }
+
+    @Test
+    fun addElement_notEmptyLabel() {
+        val evaluator = Evaluator("1")
+        assertEquals("1+", evaluator.addElement('+'))
+    }
+
+    @Test
+    fun removeLastElement_emptyLabel() {
+        val evaluator = Evaluator("")
+        assertEquals("", evaluator.removeLastElement())
+    }
+
+    @Test
+    fun removeLastElement_notEmptyLabel() {
+        val evaluator = Evaluator("1+")
+        assertEquals("1", evaluator.removeLastElement())
+    }
+
+    @Test
+    fun clear_emptyLabel() {
+        val evaluator = Evaluator("")
+        assertEquals("", evaluator.clear())
+    }
+
+    @Test
+    fun clear_notEmptyLabel() {
+        val evaluator = Evaluator("1")
+        assertEquals("", evaluator.clear())
+    }
+
+    @Test
+    fun eval__addition() {
+        val evaluator = Evaluator("1+1")
+        assertEquals("2.0", evaluator.eval())
+    }
+
+    @Test
+    fun eval__substraction() {
+        val evaluator = Evaluator("2-1")
+        assertEquals("1.0", evaluator.eval())
+    }
+
+    @Test
+    fun eval__multiply() {
+        val evaluator = Evaluator("2*3")
+        assertEquals("6.0", evaluator.eval())
+    }
+
+    @Test
+    fun eval__divide() {
+        val evaluator = Evaluator("20/5")
+        assertEquals("4.0", evaluator.eval())
+    }
+
+    @Test
+    fun eval__error() {
+        val evaluator = Evaluator("1/")
+        assertEquals("Error", evaluator.eval())
+    }
+
+}
